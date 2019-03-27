d5d90c200e420cb1c373c210a850d98345c58c67
Scott Dowding <scottddowding1279@gmail.com>
2019-03-27 11:31:21 -0400

    first set of object drills

---
+++ objects.js
@@ -0,0 +1,61 @@
+'use strict';
+
+function createMyObject() {
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function () {
+      return 'hello';
+    }
+  };
+}
+
+// console.log(createMyObject());
+
+function updateObject(obj) {
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
+}
+
+const oldObj = {
+  cats: 'cats',
+  dogs: 'dogs',
+};
+const newObj = updateObject(oldObj);
+
+// console.log(newObj);
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function () {
+      return `${this.firstName} ${this.lastName}`;
+    }
+  };
+  return person;
+}
+
+const person = personMaker();
+// console.log(person.fullName());
+
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
+
+console.log(keyDeleter(sampleObj));
